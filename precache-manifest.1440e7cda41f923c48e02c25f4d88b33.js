f0e0b69829dabae2ef1c310b9bad5e3f6de553eb
AlexanderDergachev <31667079+AlexanderDergachev@users.noreply.github.com>
2020-02-05 09:40:08 +0300

    Updates

---
+++ precache-manifest.1440e7cda41f923c48e02c25f4d88b33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95da1af530e1df2fcce32f0314de02f",
+    "url": "/react-redux-trello-like/index.html"
+  },
+  {
+    "revision": "0b891991d30bb5d520de",
+    "url": "/react-redux-trello-like/static/css/2.8953b94b.chunk.css"
+  },
+  {
+    "revision": "209301d769ff98738c93",
+    "url": "/react-redux-trello-like/static/css/main.e0ecc367.chunk.css"
+  },
+  {
+    "revision": "0b891991d30bb5d520de",
+    "url": "/react-redux-trello-like/static/js/2.6f57c4a6.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-redux-trello-like/static/js/2.6f57c4a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "209301d769ff98738c93",
+    "url": "/react-redux-trello-like/static/js/main.ec9a8109.chunk.js"
+  },
+  {
+    "revision": "259b9aaa8c0a0fa10d8b",
+    "url": "/react-redux-trello-like/static/js/runtime-main.a9e11709.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-redux-trello-like/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-redux-trello-like/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-redux-trello-like/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-redux-trello-like/static/media/slick.f97e3bbf.svg"
+  }
+]);
